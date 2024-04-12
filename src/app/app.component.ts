2e3c7e7b5ede3b6c50f9f3316cb11af50a289d17
joelwwo <joeldossantossilva21@gmail.com>
2024-04-12 09:06:12 -0300

    Adição do serviço Marvel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MarvelService } from './services/marvel.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tic-tac-toe';
+  resultFromSearch: any;
+  search = new FormControl('');
+
+  constructor(private marvelService: MarvelService) {}
+
+  ngOnInit() {
+    this.marvelService.getCharacters('w').subscribe((res: any) => {
+      this.resultFromSearch = res.data;
+    });
+  }
 }
