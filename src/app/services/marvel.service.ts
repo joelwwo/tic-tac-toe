d309778a6aedcd0d5ef44ccb74ce6e4e079e28e3
joelwwo <joeldossantossilva21@gmail.com>
2024-04-13 13:47:28 -0300

    Retirada de comentários

--- src/app/services/marvel.service.ts
+++ src/app/services/marvel.service.ts
@@ -27,13 +27,7 @@ export class MarvelService {
 
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
       alert(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
       return of(result as T);
     };
   }
