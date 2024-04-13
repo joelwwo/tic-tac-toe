7fdb811bdd6233d0ff562ba73a73b30718c71f20
joelwwo <joeldossantossilva21@gmail.com>
2024-04-13 11:30:59 -0300

    Alteração da importação

--- src/app/services/marvel.service.ts
+++ src/app/services/marvel.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import MD5 from 'crypto-js/MD5';
+import crypto from 'crypto-js';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -18,9 +18,9 @@ export class MarvelService {
     const publicKey = environment.marvelPublicKey;
     const privateKey = environment.marvelPrivateKey;
     const privateKeyEncoded = encodeURIComponent(privateKey);
-    const hash = MD5(
-      encodeURIComponent(ts + privateKeyEncoded + publicKey)
-    ).toString();
+    const hash = crypto
+      .MD5(encodeURIComponent(ts + privateKeyEncoded + publicKey))
+      .toString();
     const query = `?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
     return query;
   }
