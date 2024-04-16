f606278be21be409d8322c7465338d8cefaf43f2
joelwwo <joeldossantossilva21@gmail.com>
2024-04-15 21:58:42 -0300

    Retirada de logs e comentários

--- src/app/services/tic-tac-toe/tic-tac-toe.service.ts
+++ src/app/services/tic-tac-toe/tic-tac-toe.service.ts
@@ -20,30 +20,11 @@ export class TicTacToeService {
     [2, 4, 6],
   ];
 
-  constructor() {}
-
-  // const checkWinner = () => {
-  //   for (let condition of winningConditions) {
-  //     const [a, b, c] = condition;
-  //     if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
-  //       gameActive = false;
-  //       return gameState[a];
-  //     }
-  //   }
-  //   if (!gameState.includes('')) {
-  //     gameActive = false;
-  //     return 'draw';
-  //   }
-  //   return null;
-  // };
-
   resetGameState(): void {
     this.gameState = ['', '', '', '', '', '', '', '', ''];
   }
 
   checkWinner(): IResultOfThePlay {
-    console.log('gameState: ', this.gameState);
-
     const gameState = this.gameState as ICharacter[];
     const incompleteTable = this.gameState.includes('');
 
@@ -60,10 +41,7 @@ export class TicTacToeService {
       }
     }
 
-    if (incompleteTable) {
-      console.log("gameState.includes(''): ", this.gameState.includes(''));
-      return { result: 'in-progress' };
-    }
+    if (incompleteTable) return { result: 'in-progress' };
 
     return {
       result: 'draw',
@@ -73,6 +51,5 @@ export class TicTacToeService {
   handleClick(index: number): void {
     if (this.gameState[index] || this.currentPlayer.canPlay === false) return;
     this.gameState[index] = this.currentPlayer;
-    // this.checkWinner();
   }
 }
