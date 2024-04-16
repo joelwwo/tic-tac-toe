4db8a2358ab62e1c26fcaaaef58c3e4008f98a6c
joelwwo <joeldossantossilva21@gmail.com>
2024-04-15 23:15:13 -0300

    Retirada de comentário

--- src/app/Mocks/MCharacters.ts
+++ src/app/Mocks/MCharacters.ts
@@ -4,7 +4,6 @@ export const MChatacters: ICharacter[] = [
   {
     id: 1009718,
     name: 'Wolverine',
-    // resourceURI: 'http://gateway.marvel.com/v1/public/characters/1009718',
     thumbnail: {
       path: 'http://i.annihil.us/u/prod/marvel/i/mg/2/60/537bcaef0f6cf',
       extension: 'jpg',
@@ -16,7 +15,6 @@ export const MChatacters: ICharacter[] = [
   {
     id: 1009412,
     name: 'Madame Hydra',
-    // resourceURI: 'http://gateway.marvel.com/v1/public/characters/1009412',
     thumbnail: {
       path: 'http://i.annihil.us/u/prod/marvel/i/mg/c/20/4d5c39a54b1dd',
       extension: 'jpg',
