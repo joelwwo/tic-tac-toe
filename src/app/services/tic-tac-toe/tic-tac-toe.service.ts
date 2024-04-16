894e0e6856df08d11969ab7ca9b3402d64a102a8
joelwwo <joeldossantossilva21@gmail.com>
2024-04-15 21:25:48 -0300

    Melhorias

--- src/app/services/tic-tac-toe/tic-tac-toe.service.ts
+++ src/app/services/tic-tac-toe/tic-tac-toe.service.ts
@@ -8,7 +8,6 @@ import { IResultOfThePlay } from 'src/app/interfaces/IResultOfThePlay';
 export class TicTacToeService {
   @Input() currentPlayer!: ICharacter;
   gameState: any = ['', '', '', '', '', '', '', '', ''];
-  gameActive = true;
 
   winningConditions = [
     [0, 1, 2],
@@ -39,8 +38,9 @@ export class TicTacToeService {
   // };
 
   checkWinner(): IResultOfThePlay {
-    if (this.gameState.includes('')) {
-      this.gameActive = false;
+    const incompleteTable = this.gameState.includes('');
+
+    if (incompleteTable) {
       console.log("gameState.includes(''): ", this.gameState.includes(''));
       return { result: 'in-progress' };
     }
@@ -52,7 +52,6 @@ export class TicTacToeService {
         this.gameState[a] === this.gameState[b] &&
         this.gameState[a] === this.gameState[c]
       ) {
-        this.gameActive = false;
         return {
           result: 'winner-defined',
         };
@@ -69,7 +68,7 @@ export class TicTacToeService {
   }
 
   handleClick(index: number): void {
-    if (this.gameState[index] || !this.gameActive) return;
+    if (this.gameState[index] || this.currentPlayer.canPlay === false) return;
     this.gameState[index] = this.currentPlayer;
   }
 }
